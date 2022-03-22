8cea9197f4f74396c3c0a1ff1ccf1b2af0bf027b
[Ezequiel Centofanti] <[ezecentofanti@gmail.com]>
2022-03-22 17:16:13 +0100

    added ground truth tracking and metrics

--- src/scripts/demo_folder.py
+++ src/scripts/demo_folder.py
@@ -97,6 +97,10 @@ class GoturnTracker:
         loader = loadfromfolder(args.input)
         self._vid_frames = loader.get_video_frames()
 
+        # added ground_truth_masks
+        loader_mask = loadfromfolder(args.ground_truth)
+        self._mask_frames = loader_mask.get_video_frames()
+
         model_dir = Path(args.model_dir)
         # Checkpoint path
         ckpt_dir = model_dir.joinpath('checkpoints')
@@ -195,37 +199,114 @@ class GoturnTracker:
         im = image_io.image_to_tensor(im)
         return im
 
+    # Added get bounding box from masks
+    def get_bb(self, png_mask, pix_oversize):
+        _, thresh = cv2.threshold(png_mask, 127, 255, 0)
+        contours,_ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) # not copying here will throw an error
+        
+        # Concatenate all contours
+        contours = np.concatenate(contours)
+
+        # Determine and draw bounding rectangle
+        x_, y_, h_, w_ = cv2.boundingRect(contours)
+
+        x1 = x_
+        y1 = y_
+        x2 = x_+h_
+        y2 = y_+w_
+        d_px = pix_oversize
+        bbox = BoundingBox(x1-d_px, y1-d_px, x2+d_px, y2+d_px)
+        return bbox
+
+    def bb_intersection_over_union(self, boxA, boxB):
+        # determine the (x, y)-coordinates of the intersection rectangle
+        xA = max(boxA.x1, boxB.x1)
+        yA = max(boxA.y1, boxB.y1)
+        xB = min(boxA.x2, boxB.x2)
+        yB = min(boxA.y2, boxB.y2)
+
+        # compute the area of intersection rectangle
+        interArea = (xB - xA) * (yB - yA)
+
+        # compute the area of both the prediction and ground-truth
+        # rectangles
+        boxAArea = (boxA.x2 - boxA.x1) * (boxA.y2 - boxA.y1)
+        boxBArea = (boxB.x2 - boxB.x1) * (boxB.y2 - boxB.y1)
+
+        # compute the intersection over union by taking the intersection
+        # area and dividing it by the sum of prediction + ground-truth
+        # areas - the interesection area
+        iou = interArea / float(boxAArea + boxBArea - interArea)
+
+        # return the intersection over union value
+        return iou
+
+    def bb_centroid_dist(self, boxA, boxB):
+        center_A_x = (boxA.x1 + boxA.x2)/2
+        center_A_y = (boxA.y1 + boxA.y2)/2
+        center_B_x = (boxB.x1 + boxB.x2)/2
+        center_B_y = (boxB.y1 + boxB.y2)/2       
+        return np.sqrt((center_A_x-center_B_x)**2 + (center_A_y-center_B_y)**2)
+        
     def track(self):
         """Track"""
         vid_frames = self._vid_frames[0]
         num_frames = len(vid_frames)
         f_path = vid_frames[0]
-        frame_0 = image_io.load(f_path)
 
-        prev = np.asarray(frame_0)
+        # Added mask frames in tracking
+        mask_frames = self._mask_frames[0]
+        m_path = mask_frames[0]
+        mask_0 = cv2.imread(m_path,cv2.IMREAD_GRAYSCALE)#[::-1] # not working
+        bbox_0 = self.get_bb(np.asarray(mask_0), pix_oversize=15)
+        
+        frame_0 = image_io.load(f_path)
+        prev = np.asarray(frame_0)#[::-1] #not working
         global image
         image = prev
+
         while True:
             # prev_out = cv2.cvtColor(prev, cv2.COLOR_RGB2BGR)
             prev_out = np.copy(prev)
             cv2.imshow('image', prev_out)
+
+
+            # Added draw bounding box ground truth
+            curr_dbg = np.copy(prev_out)
+            curr_dbg = cv2.rectangle(curr_dbg, (int(bbox_0.x1),
+                                                int(bbox_0.y1)),
+                                     (int(bbox_0.x2), int(bbox_0.y2)), (255, 255, 0), 2)
+            cv2.imshow('image', curr_dbg)
+            cv2.waitKey(20)
+
             key = cv2.waitKey(1) & 0xFF
             if key == ord('s'):
-                (x1, y1), (x2, y2) = refPt[0], refPt[1]
-                bbox_0 = BoundingBox(x1, y1, x2, y2)
+                # (x1, y1), (x2, y2) = refPt[0], refPt[1]
+                #bbox_0 = BoundingBox(x1, y1, x2, y2)
                 break
             elif key == ord('r'):
-                (x1, y1), (x2, y2) = refPt[0], refPt[1]
-                bbox_0 = BoundingBox(x1, y1, x2, y2)
+                #(x1, y1), (x2, y2) = refPt[0], refPt[1]
+                #bbox_0 = BoundingBox(x1, y1, x2, y2)
                 break
+        
 
         for i in range(1, num_frames):
             f_path = vid_frames[i]
             frame_1 = image_io.load(f_path)
-            curr = np.asarray(frame_1)
+
+            # Added masks in each iteration
+            m_path = mask_frames[i]
+            mask_1 = cv2.imread(m_path,cv2.IMREAD_GRAYSCALE)#[::-1] # not working
+            bbox_true = self.get_bb(np.asarray(mask_1), pix_oversize=0)
+            
+            curr = np.asarray(frame_1)#[::-1]
             bbox_0 = self._track(curr, prev, bbox_0)
             bbox = bbox_0
             prev = curr
+
+            IOU_val = self.bb_intersection_over_union(bbox_true, bbox)
+            centroid_dist = self.bb_centroid_dist(bbox_true, bbox)
+            print(centroid_dist)
 
             if cv2.waitKey(1) & 0xFF == ord('p'):
                 while True:
@@ -241,6 +322,10 @@ class GoturnTracker:
             curr_dbg = cv2.rectangle(curr_dbg, (int(bbox.x1),
                                                 int(bbox.y1)),
                                      (int(bbox.x2), int(bbox.y2)), (255, 255, 0), 2)
+            # draw ground truth
+            curr_dbg = cv2.rectangle(curr_dbg, (int(bbox_true.x1),
+                                                int(bbox_true.y1)),
+                                     (int(bbox_true.x2), int(bbox_true.y2)), (0, 255, 255), 1)
 
             # curr_dbg = cv2.cvtColor(curr_dbg, cv2.COLOR_RGB2BGR)
             cv2.imshow('image', curr_dbg)
@@ -254,6 +339,9 @@ if __name__ == "__main__":
                     required=True, help='path to input folder containing all the frames')
     ap.add_argument('--model_dir',
                     required=True, help='model directory')
+    # Added argument for masks directory
+    ap.add_argument('--ground_truth',
+                    required=True, help='ground truth masks directory')
 
     args = ap.parse_args()
     objG = GoturnTracker(args, dbg=False)
--- src/scripts/demo_folder.sh
+++ src/scripts/demo_folder.sh
@@ -1,5 +1,7 @@
-MODEL_DIR='../../models/caffenet/lightning_logs/version_0/'
-FOLDER='../../test/8/'
+MODEL_DIR='../goturn/models/caffenet/lightning_logs/version_0/'
+FOLDER='../../test/bag_jpg/'
+MASKS='../../test/bag_png/'
 
 python demo_folder.py --model_dir $MODEL_DIR\
 	--input $FOLDER \
+	--ground_truth $MASKS \
